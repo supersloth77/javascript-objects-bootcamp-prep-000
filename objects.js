5fa4feac7b273027a7c0c47c6640eed124532773
Peter Traversa <peter.traversa@gmail.com>
2018-06-30 00:40:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,5 @@ function updatePlaylist(playlist, artistName, songTitles) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.["artistName"];
+  delete playlist.[artistName];
 }
